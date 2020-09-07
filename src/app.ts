212d75f8124f9fde00e89b983f2b8a1e1955e916
kundan <kundankumar594@gmail.com>
2020-09-07 09:52:23 -0700

    cors issuess

--- src/app.ts
+++ src/app.ts
@@ -16,28 +16,12 @@ class App {
 
     private initializeMiddlewares() {
         this.app.use(bodyParser.json());
-        this.app.use(cors({
-            origin: '*'
-        }));
-        this.app.use(function (req, res, next) {
-            console.log("====== dataad tes");
-
-            // Website you wish to allow to connect
-            res.setHeader('Access-Control-Allow-Origin', '*');
-        
-            // Request methods you wish to allow
-            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-        
-            // Request headers you wish to allow
-            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-        
-            // Set to true if you need the website to include cookies in the requests sent
-            // to the API (e.g. in case you use sessions)
-            res.setHeader('Access-Control-Allow-Credentials', 'true');
-        
-            // Pass to next layer of middleware
+       // this.app.use(cors());
+        this.app.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             next();
-        });
+          });
     }
 
     private initializeControllers(controllers: any[]) {
