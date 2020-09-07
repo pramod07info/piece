7d1c0bb3010e0652a3b965c86ff5c937882c65b7
TUSHAR KHARAT <kharat.tushar11@gmail.com>
2020-09-07 16:57:01 +0530

    fixing cors

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import * as bodyParser from 'body-parser';
 
-
+const allowedOrigins:any = ['http://localhost:3000',
+                      'https://sofadog-newsflare.vercel.app'];
 
 class App {
     public app: express.Application;
@@ -13,17 +14,31 @@ class App {
         optionsSuccessStatus: 200
     }
 
+    
+
     constructor(controllers: any, port: number) {
         this.app = express();
         this.port = port;
-        this.app.options('*', cors()) ;
-
+        
         this.initializeMiddlewares();
         this.initializeControllers(controllers);
     }
 
     private initializeMiddlewares() {
         this.app.use(bodyParser.json());
+        this.app.use(cors({
+            origin: function(origin, callback){
+              // allow requests with no origin 
+              // (like mobile apps or curl requests)
+              if(!origin) return callback(null, true);
+              if(allowedOrigins.indexOf(origin) === -1){
+                var msg = 'The CORS policy for this site does not ' +
+                          'allow access from the specified Origin.';
+                return callback(new Error(msg), false);
+              }
+              return callback(null, true);
+            }
+          }));
     }
 
     private initializeControllers(controllers: any[]) {
