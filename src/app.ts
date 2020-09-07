0f43c13a2d7b752b6c3c35b1b49d4eb73d483d1f
kundan <kundankumar594@gmail.com>
2020-09-07 09:36:46 -0700

    header modified for cors

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,25 @@ class App {
         this.app.use(cors({
             origin: '*'
         }));
+        this.app.use(function (req, res, next) {
+            console.log("====== dataad tes");
+
+            // Website you wish to allow to connect
+            res.setHeader('Access-Control-Allow-Origin', 'https://sofadog-newsflare.vercel.app');
+        
+            // Request methods you wish to allow
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        
+            // Request headers you wish to allow
+            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        
+            // Set to true if you need the website to include cookies in the requests sent
+            // to the API (e.g. in case you use sessions)
+            res.setHeader('Access-Control-Allow-Credentials', 'true');
+        
+            // Pass to next layer of middleware
+            next();
+        });
     }
 
     private initializeControllers(controllers: any[]) {
