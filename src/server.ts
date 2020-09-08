ad7f531761f01d6e1fa7336da33c8da8f2e064e3
TUSHAR KHARAT <kharat.tushar11@gmail.com>
2020-09-09 00:59:55 +0530

    upadte

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,6 @@ import VideoInfoController from './controller/video_info_controller';
 import SentencesController from './controller/sentences-controller';
 import SourceController from './controller/source_controller';
 
-
 const PORT : any = process.env.PORT || 8888;
 const app = new App(
   [
